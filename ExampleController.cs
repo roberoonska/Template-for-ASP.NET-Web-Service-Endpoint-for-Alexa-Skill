7f11a73f4977e877f814189da22a3b5c9f42207f
roberoonska <rhw225@cornell.edu>
2019-05-21 15:50:34 -0400

    Update ExampleController.cs

--- ExampleController.cs
+++ ExampleController.cs
@@ -7,6 +7,7 @@ using Alexa.NET.Request;
 using Alexa.NET.Response;
 
 
+
 namespace YOURPROJECTNAME.Controllers
 {
     public class YOURCONTROLLERNAME : ApiController
@@ -14,20 +15,14 @@ namespace YOURPROJECTNAME.Controllers
         [HttpPost]
         public async Task<Object> AlexaResponseAsync(SkillRequest skillRequest)
         {
-            
             SkillResponse response = new SkillResponse();
-
             int securityCode = await SecurityHelper.SecurityHandler(Request, skillRequest);
-
             if (securityCode == 1) //request not validated
                 return BadRequest();
-
             else if (securityCode == 2) //request ID doesn't match
                 response.Response = new ResponseBody() { ShouldEndSession = true };
-
             else
                 response = RequestHandler(skillRequest);
-
             return response;
         }
         
@@ -40,7 +35,6 @@ namespace YOURPROJECTNAME.Controllers
         public static SkillResponse RequestHandler(SkillRequest skillRequest)
         {
             SkillResponse response = new SkillResponse();
-
             switch (skillRequest.Request.Type)
             {
                 case "LaunchRequest":
@@ -68,59 +62,50 @@ namespace YOURPROJECTNAME.Controllers
         public static SkillResponse IntentRequestHandler(SkillRequest skillRequest,  MessLog mess)
         {
             SkillResponse response = new SkillResponse();
-
             IntentData intentData = new IntentData(skillRequest, mess);
             switch (intentData.IntentName)
             {
                 case "YOURCUSTOMINTENTNAME1":
                     //YOUR CUSTOM INTENT CODE HERE
                     break;
-                    
                 case "YOURCUSTOMINTENTNAME2":
                     //YOUR CUSTOM INTENT CODE HERE
-                    break;
-										
-								case "AMAZON.HelpIntent":
+                    break;							
+		        case "AMAZON.HelpIntent":
                     response = ResponseBuilder.Ask("RESPONSE TEXT HERE", new Reprompt("REPROMPT TEXT HERE"));
                     break;
-
                 case "AMAZON.CancelIntent":
                     response.Response = new ResponseBody() { ShouldEndSession = true };
                     break;
-
                 case "AMAZON.StopIntent":
                     response.Response = new ResponseBody() { ShouldEndSession = true };
                     break;
-
                 case "AMAZON.NavigateHomeIntent":
                     response.Response = new ResponseBody() { ShouldEndSession = true };
                     break;
-
                 case "AMAZON.FallBackIntent":
                     response = ResponseBuilder.Tell("I did not understand that. Please try again.");
                     break;	
             }
-
             return response;
-
         }
-
     }
 		
 		
 		
-		public class IntentData
+    public class IntentData
    	{
         public string IntentName { get; set; }
         private SkillRequest SkillRequest { get; set; }
         private IntentRequest IntentRequest { get; set; }
-				//YOUR INTENT DATA PARAMETERS HERE
+	    //YOUR INTENT DATA PARAMETERS HERE
 
-        public IntentData(SkillRequest skillRequest, MessLog mess)
+        public IntentData(SkillRequest skillRequest)
         {
             SkillRequest = skillRequest;
             IntentRequest = SkillRequest.Request as IntentRequest;
             IntentName = IntentRequest.Intent.Name;
+            
         }
-		}
+    }
 }
